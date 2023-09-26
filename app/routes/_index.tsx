0c6702a65fa119eaa19b54738f22663d45276a90
Raul de Melo <melo.raulf@gmail.com>
2023-09-26 13:04:19 +0200

    add example

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,61 @@
-import type { MetaFunction } from "@remix-run/node";
+import { type ActionFunctionArgs, json } from "@remix-run/node";
+import * as zod from "zod";
+import { useRemixForm, getValidatedFormData } from "remix-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form } from "@remix-run/react";
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+const schema = zod.object({
+  name: zod.string().nonempty(),
+  email: zod.string().email().nonempty(),
+});
+
+type FormData = zod.infer<typeof schema>;
+
+const resolver = zodResolver(schema);
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const {
+    errors,
+    data,
+    receivedValues: defaultValues,
+  } = await getValidatedFormData<FormData>(request, resolver);
+  if (errors) {
+    // The keys "errors" and "defaultValue" are picked up automatically by useRemixForm
+    return json({ errors, defaultValues });
+  }
+
+  // Do something with the data
+  return json(data);
 };
 
 export default function Index() {
+  const {
+    handleSubmit,
+    formState: { errors },
+    register,
+  } = useRemixForm({
+    mode: "onSubmit",
+    resolver,
+  });
+
+  console.log(errors);
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <Form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" {...register("name")} />
+          {errors.name && <p>{errors.name.message as string}</p>}
+        </label>
+        <label>
+          Email:
+          <input type="email" {...register("email")} />
+          {errors.email && <p>{errors.email.message as string}</p>}
+        </label>
+        <button type="submit">Submit</button>
+      </Form>
     </div>
   );
 }
